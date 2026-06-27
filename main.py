b9639df1f4ea28dab452aa8b8a69dc979eb23592
Devi Sri Macharla <devisri.macharla7@gmail.com>
2026-06-27 17:46:33 -0400

    Add PDF upload API and update project configuration

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
@@ -19,4 +24,20 @@ def about():
     return {
         "project": "RAG Regulatory Document Search",
         "developer": "Devi Sri Macharla"
+    }
+
+@app.post("/upload")
+async def upload_document(file: UploadFile = File(...)):
+    
+    file_path = os.path.join(
+        UPLOAD_FOLDER,
+        file.filename
+    )
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+    return {
+        "filename": file.filename,
+        "status": "uploaded successfully"
     }
